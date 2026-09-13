745f5f3e9d2d8252e153deb9f1eb66c99115507b
Nikhil-0084 <xnikhilthakur421@gmail.com>
2026-09-13 16:44:40 +0530

    Time: 40 ms (55.73%) | Memory: 85.6 MB (99.52%) - LeetSync

--- 164-maximum-gap/maximum-gap.cpp
+++ 164-maximum-gap/maximum-gap.cpp
@@ -6,9 +6,9 @@ public:
       int maxDiff=0;
       sort(nums.begin(),nums.end());
       for(int i=0;i<n-1;i++){
-        long long diff=nums[i+1]-nums[i];
-        int curr=diff;
-        maxDiff=max(maxDiff,curr);
+        int  diff=nums[i+1]-nums[i];
+       
+        maxDiff=max(maxDiff,diff);
       }
       return maxDiff;
     }
